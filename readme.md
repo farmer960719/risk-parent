51785f3da2d0ea2f98e53f46c9e9f3a3875c9ee6
liuscoding <liushuai1@ysstech.com>
2020-09-14 14:31:31 +0800

    搭建demo架构

---
+++ readme.md
@@ -0,0 +1,7 @@
+#  demo架构
+
+## risk-parent
+
+作为demo的父pom  只做依赖管理   不做代码编写
+
+编写demo，选中risk-parent，右键新建module，不需要导入依赖，父pom全部都有
---
+++ risk-demo/src/main/java/com/yss/risk/App.java
@@ -0,0 +1,13 @@
+package com.yss.risk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] rgs) {
+        SpringApplication.run(App.class,rgs);
+    }
+}
